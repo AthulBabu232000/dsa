f132b4933b8c804e455fd45c922efa066473b897
asbupsilonno1 <athul.babu.2300@gmail.com>
2023-07-18 20:25:34 +0530

    mployed the classic reversal of linkedlist

--- linkedlist/reverse.py
+++ linkedlist/reverse.py
@@ -8,7 +8,7 @@ class sLinkedList:
         self.headvalue=None
 
 head=sLinkedList()
-head.headvalue=Node('a')
+head.headvalue=Node('a')    
 b=Node('b')
 c=Node('c')
 d=Node('d')
@@ -34,10 +34,14 @@ def addToList():
     return revList
 revList=addToList()
 n=len(revList)
+
+# reversing the list 
 for i in range(n//2):
     temp=revList[i]
     revList[i]=revList[n-i-1]
     revList[n-i-1]=temp
+
+# setting reversed value in array to linkedlist nodes 
 head.headvalue=revList[0]
 current=head.headvalue
 for i in revList[1:]:
@@ -46,4 +50,48 @@ for i in revList[1:]:
 current.nextvalue=None
 print("reversing the linkedlist")
 printList()
+
+
+
+
+
+
+# the classic way of reversing the linkedlist 
+
+header=Node('1')
+body1=Node('2')
+body2=Node('3')
+body3=Node('4')
+footer=Node('5')
+header.nextvalue=body1 
+body1.nextvalue=body2 
+body2.nextvalue=body3 
+body3.nextvalue=footer 
+footer.nextvalue=None 
+
+current=header 
+while(current is not None):
+    print(current.datavalue)
+    current=current.nextvalue 
+
+print("reversing the linkedlist in classic way by reversing the pointer while traversing")
+# here i use two pointers one is "prev" which is null initially and "current" points to first element in the beginning 
+prev=None 
+current=header 
+while(current is not None):
+    temp=current
+    current=current.nextvalue
+    temp.nextvalue=prev 
+    prev=temp
+# when the traversal ends prev points to the last element
+# actually their linkage is reversed 
+# finally printing to conform the reversal 
+current=prev 
+while(current is not None):
+    print(current.datavalue)
+    current=current.nextvalue 
+
+
+
+
 
