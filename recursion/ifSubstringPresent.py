1c950a0b09f4d0304b16ff7da3dd7b4d8522853a
asbupsilonno1 <athul.babu.2300@gmail.com>
2023-07-19 13:04:06 +0530

    i think it is right now for recursion

--- recursion/ifSubstringPresent.py
+++ recursion/ifSubstringPresent.py
@@ -1,12 +1,18 @@
 mainString="hellothisisthebeginningofthenewworld"
-subString="ord"
-def findString(main,sub):
+subString="orld"
+def findString(main,sub,real):
     sizeMain=len(main)
     sizeSub=len(sub)
     if sizeSub==0: return True 
     if sizeMain==0:return False 
     if main[0]==sub[0]:
-        return findString(main,sub)
-    return findString(main[1:],sub)
-result=findString(mainString,subString)
+        return findString(main[1:],sub[1:],real)
+    else:
+        sub=real
+    return findString(main[1:],sub,real)  
+result=findString(mainString,subString,subString)
 print(result)
+if subString in mainString:
+    print("yes it is present")
+else:
+    print("no not present")
