941a94a91b880462f0738d7e575bd51fcf003840
asbupsilonno1 <athul.babu.2300@gmail.com>
2023-08-12 21:16:59 +0530

    powerofthree was a menace bro

--- leetcode/326.powerofthree.py
+++ leetcode/326.powerofthree.py
@@ -1,7 +1,15 @@
+# so here i used is_integer() to check if it is an integer or not 
+# i faced an issue with the number 243 log(243,3)=>4.99999999999
+# first fix i used to round the number to 4 decimal places 
+# i made 5 submissions all failed only round(log(n,3),10)
+# only 10 decimal places works well
 from math import log
 def powerofThree(n):
     if n<=0:
         return False 
-    return log(n,3).is_integer()
+    print(log(n,3))
+    prev=round(log(n,3),10)
+    print(prev)
+    return prev.is_integer()
 output=powerofThree(int(input("enter the number to check if the power of three or not: ")))
 print(output)
